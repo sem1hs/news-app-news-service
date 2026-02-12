d6d38f0a1b14870dc76d895d230113456f2852d1
sem1hs <semihgtt11@gmail.com>
2026-02-12 11:12:51 +0300

    ayarlamalar yapıldı

--- src/main/java/com/semihsahinoglu/news_service/client/AuthClient.java
+++ src/main/java/com/semihsahinoglu/news_service/client/AuthClient.java
@@ -4,7 +4,7 @@ import com.semihsahinoglu.news_service.config.NoAuthFeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "auth-service", url = "http://localhost:8081",configuration = NoAuthFeignConfig.class)
+@FeignClient(name = "auth-service", configuration = NoAuthFeignConfig.class)
 public interface AuthClient {
 
     @PostMapping("/internal/auth/service-token")
